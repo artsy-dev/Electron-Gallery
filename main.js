f98eb0936d1136c8ff143707f6c41803836ef901
Lexouden <lexouden@outlook.com>
2019-11-12 11:27:29 +0100

    added an env var check

--- main.js
+++ main.js
@@ -30,8 +30,10 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
+  if (process.env.NODE_ENV === "development") {
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools()
+  }
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
